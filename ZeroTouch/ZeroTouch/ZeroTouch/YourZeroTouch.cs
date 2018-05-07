d8d3e7bb81bda32ae375a67bacd4fa6393426556
Summer Pi <xiatian3.1415@gmail.com>
2018-05-07 16:33:53 +0800

    Add Example File
    Simple custom node by using  Dynamo ZeroTouch

---
+++ ZeroTouch/ZeroTouch/ZeroTouch/YourZeroTouch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroTouch
+{
+    public class YourZeroTouch
+    {
+        public static int Multiplication(int a, int b)
+        {
+            return Method.Multiplication(a, b);
+        }
+    }
+}
